82e86a8424ba0f6e8a68acb7ac98254a210970d5
poros666 <790543965@qq.com>
2020-11-20 16:31:45 +0800

    补充了一部分注释

--- src/main/java/team/animal_games/competition/Running.java
+++ src/main/java/team/animal_games/competition/Running.java
@@ -4,17 +4,17 @@ import main.java.team.animal_games.score.RunningRecorder;
 import main.java.team.animal_games.score.ScoreRecorder;
 
 
-public class Running extends Competition{
+public class Running extends Competition {
 
     protected int _distance;
 
-    public Running(){
-        if(Math.random()>0.5){
+    public Running() {
+        if (Math.random() > 0.5) {
             _distance = 100;
-        }else {
+        } else {
             _distance = 1000;
         }
-        System.out.println("Running::Running()::\"create a "+_distance+" meters running game!\"");
+        System.out.println("Running::Running()::\"create a " + _distance + " meters running game!\"");
     }
 
 
@@ -29,7 +29,7 @@ public class Running extends Competition{
     }
 
     @Override
-    public int[] getScore(){
+    public int[] getScore() {
         return new RunningRecorder().recordScore(this);
     }
 }
