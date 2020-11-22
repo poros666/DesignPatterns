f014c1a2ffe1d73b70fd4f1ff081793ccc3f0724
Muxi Yan <yanmuxi58@gmail.com>
2020-11-22 19:24:33 +0800

    ceremonys

---
+++ ceremony/StagePositionHandler.java
@@ -0,0 +1,19 @@
+class StagePositionHandler extends PositionHandler
+{
+    public StagePositionHandler()
+    {
+        super(new Position("stage"));
+    }
+
+    public StagePositionHandler(Position p)
+    {
+        super(p);
+    }
+
+    @Override
+    public void handle(Participant p) {
+        // TODO Auto-generated method stub
+        System.out.println("stage handling");
+        super.handle(p);
+    }
+}
