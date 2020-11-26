c400813934a9fd3fe5517b711be1a27d56aed89f
sgSillage <51256576+sgSillage@users.noreply.github.com>
2020-11-26 16:30:56 +0800

    TransferObject Pattern Add

---
+++ src/main/java/team/animal_games/Animal/TransferObject/AnimalBO.java
@@ -0,0 +1,73 @@
+package main.java.team.animal_games.Animal.TransferObject;
+
+import main.java.team.animal_games.Animal.Animal;
+import main.java.team.animal_games.state.StateExcited;
+import main.java.team.animal_games.state.StateNormal;
+import main.java.team.animal_games.state.StateSick;
+import main.java.team.animal_games.state.StateStarve;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class AnimalBO {
+
+    List<Animal> animals;
+
+    public AnimalBO(){
+        System.out.println("AnimalBO: AnimalBO(): create a database and add some data.");
+        animals = new ArrayList<Animal>();
+        Animal animal1 = new Animal(new StateNormal(),"tiger");
+        Animal animal2 = new Animal(new StateNormal(),"lion");
+        animals.add(animal1);
+        animals.add(animal2);
+    }
+
+    public void deleteAnimals(String n){
+        animals.remove(this.getAniaml(n));
+    }
+
+    public List<Animal> getAllAnimals(){
+        System.out.println("AnimalBO: getAllAnimals(): get all animals' data in the DB.");
+        return animals;
+    }
+
+    public Animal getAniaml(String n){
+        System.out.println("AnimalBO: getAnimal(): get animal in DB by name.");
+        for(Animal animal:animals){
+            if(animal.getName().equals(n)){
+                return animal;
+            }
+        }
+        return null;
+    }
+
+
+    public void updateAnimal(Animal animal){
+        System.out.println("AnimalBo: updateAnimal(): update animals' state");
+        this.getAniaml(animal.getName()).setState(animal.getState());
+        System.out.println("Animal: Name = "
+                + animal.getName() +", updated in the database");
+    }
+
+    public void printAnimal(Animal an){
+        System.out.print(this.getAllAnimals().indexOf(an)+" Animal :Name: " +
+                an.getName() +
+                "  State : [ ");
+        if (StateNormal.class.equals(an.getState().getClass())) {
+            System.out.print("Normal ]");
+        } else if (StateExcited.class.equals(an.getState().getClass())) {
+            System.out.print("Excited ]");
+        } else if (StateSick.class.equals(an.getState().getClass())) {
+            System.out.print("Sick ]");
+        } else if (StateStarve.class.equals(an.getState().getClass())) {
+            System.out.print("Starve ]");
+        }
+        System.out.print("  Attribute: [ \n");
+        for (Map.Entry<String,Integer> entry:an.getAttribute().entrySet()){
+            System.out.print(entry.getKey()+":"+entry.getValue()+"  ");
+        }
+        System.out.println("]\n");
+    }
+}
---
+++ src/main/java/team/animal_games/Animal/TransferObject/TestTransferObject.java
@@ -0,0 +1,63 @@
+package main.java.team.animal_games.Animal.TransferObject;
+
+import main.java.team.animal_games.Animal.Animal;
+import main.java.team.animal_games.state.StateExcited;
+import main.java.team.animal_games.state.StateNormal;
+import main.java.team.animal_games.state.StateSick;
+import main.java.team.animal_games.state.StateStarve;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class TestTransferObject {
+    public static void main(String[] arg){
+        AnimalBO animalBO = new AnimalBO();
+
+        System.out.println("—————————————-------------------------------------———— Test[Transfer Object]Pattern —————————————-------------------------------------————");
+        System.out.println("Follow are (classname: methods: action)！"+'\n');
+
+        System.out.println("AnimalBO: AnimalBO(): create a database and add some data.");
+        System.out.println("AnimalBO: getAllAnimals(): get all animals' data in the DB.");
+        System.out.println("AnimalBO: getAnimal(): get animal in DB by name.");
+        System.out.println("AnimalBo: updateAnimal(): update animals' state\n");
+
+        //输出所有动物
+        for (Animal animal : animalBO.getAllAnimals()){
+            animalBO.printAnimal(animal);
+        }
+
+        //更新动物
+        Scanner reader = new Scanner(System.in);
+        System.out.println("----------------now let update animals---------------");
+        System.out.println("--------------choose the index of Animal-------------");
+        int index = reader.nextInt();
+        Animal temp = animalBO.getAllAnimals().get(index);
+        System.out.println("state:1 normal|2 excited|3 sick|4 starve");
+        int state = reader.nextInt();
+        switch (state){
+            case 1:
+                temp.setState(new StateNormal());
+                break;
+            case 2:
+                temp.setState(new StateExcited());
+                break;
+            case 3:
+                temp.setState(new StateSick());
+                break;
+            case 4:
+                temp.setState(new StateStarve());
+                break;
+            default:break;
+        }
+        animalBO.updateAnimal(temp);
+
+        //获取动物
+        System.out.println("-----------------now let get animal-----------------");
+        System.out.println("---------------input the name of Animal-------------");
+        String na = reader.next();
+        Animal animal = animalBO.getAniaml(na);
+        animalBO.printAnimal(animal);
+
+        System.out.println("—————————————---------------------------------------------- End ————------—————————-------------------------------------————");
+    }
+}
